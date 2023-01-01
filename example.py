c7d509ca674aec722932f1a9301308e37daff5e9
Mike Kinney <mike.kinney@gmail.com>
2023-01-01 14:40:50 -0800

    add example

---
+++ example.py
@@ -0,0 +1,13 @@
+#!python
+
+from otp import OTP
+from key import Key
+
+alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+text = "HELLO"
+k = Key(alphabet=alphabet, length=len(text))
+mykey=k.generated
+a = OTP(alphabet=alphabet, key=mykey)
+_, encoded = a.encode(text)
+print("text:", text, "key:", mykey, "encoded:", encoded)
